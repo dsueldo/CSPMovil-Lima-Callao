9b715aea1dc92929f141fc517f58d31c3f2e041e
Diego Sueldo <diego_sueldo5@hotmail.com>
2026-05-12 03:39:12 -0500

    feat: Update versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,8 @@ android {
         applicationId = "com.colegiosociologosperu.cspmovillimacallao"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-
-        versionName = "1.4"
+        versionCode = 14
+        versionName = "1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
